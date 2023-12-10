307309ed9333ed28a6d85c5a01e6d0ddbca1cc89
dongyuanhu <dongyuanhu@tencent.com>
2023-12-10 15:36:51 +0800

    add recursion sum

---
+++ recursion/sum.cpp
@@ -0,0 +1,24 @@
+#include "core.hpp"
+#include <iostream>
+
+void sum_parameter(int i, int &sum) {
+  if (i <= 0)
+    return;
+  sum += i;
+  sum_parameter(i - 1, sum);
+}
+
+int sum_func(int n) {
+  if (n <= 0)
+    return 0;
+  return n + sum_func(n - 1);
+}
+
+int main() {
+  int sum = 0, n = 10;
+  sum_parameter(n, sum);
+  std::cout << "sum_parameter:" << sum << "\n";
+  print_seperator();
+  std::cout << "sum:" << sum_func(n) << "\n";
+  return 0;
+}
