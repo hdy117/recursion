a67efc259e6d875361716919f66fe826ad48b691
dongyuanhu <dongyuanhu@tencent.com>
2023-12-10 14:04:57 +0800

    print name for n times

---
+++ recursion/print_names.cpp
@@ -0,0 +1,15 @@
+#include "core.hpp"
+#include <iostream>
+
+void print_names(int n, const std::string &content) {
+  if (n <= 0)
+    return;
+
+  std::cout << content << "\n";
+  print_names(n - 1, content);
+}
+
+int main() {
+  print_names(3, "Hi");
+  return 0;
+}
