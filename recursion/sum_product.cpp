0409f907483378903da64adbabeeb637a5cb487a
dongyuanhu <dongyuanhu@tencent.com>
2023-12-10 15:52:05 +0800

    add product

--- recursion/sum_product.cpp
+++ recursion/sum_product.cpp
@@ -14,11 +14,19 @@ int sum_func(int n) {
   return n + sum_func(n - 1);
 }
 
+int product(int n) {
+  if (n <= 0)
+    return 1;
+  return n * product(n - 1);
+}
+
 int main() {
   int sum = 0, n = 10;
   sum_parameter(n, sum);
   std::cout << "sum_parameter:" << sum << "\n";
   print_seperator();
   std::cout << "sum:" << sum_func(n) << "\n";
+  print_seperator();
+  std::cout << "product:" << product(3) << "\n";
   return 0;
 }
