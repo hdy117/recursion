db391a4c685507da72e673c33e0849a52b517b0d
dongyuanhu <dongyuanhu@tencent.com>
2023-12-10 13:26:15 +0800

    add stackoverflow recursion

---
+++ recursion/stackoverflow.cpp
@@ -0,0 +1,16 @@
+#include "core.hpp"
+#include <iostream>
+
+/**
+ * @brief no stop condition
+ *
+ */
+void stackoverflow() {
+  std::cout << "called again.\n";
+  stackoverflow();
+}
+
+int main() {
+  stackoverflow();
+  return 0;
+}
