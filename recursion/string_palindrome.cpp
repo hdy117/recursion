0cceaaec909ffa5934c3a008d98d6b18dca15e9d
dongyuanhu <dongyuanhu@tencent.com>
2023-12-10 17:15:14 +0800

    update pardilindrome

--- recursion/string_palindrome.cpp
+++ recursion/string_palindrome.cpp
@@ -11,6 +11,14 @@ bool is_palindrome(const std::string &str, int left, int right) {
          is_palindrome(str, left + 1, right - 1);
 }
 
+bool is_palindrome_a(const std::string &str, int left, int right) {
+  if (left >= right)
+    return true;
+  if (str.at(left) != str.at(right))
+    return false;
+  return is_palindrome_a(str, left + 1, right - 1);
+}
+
 int main() {
   std::string str_a("madam");
   std::string str_b("madama");
@@ -19,5 +27,10 @@ int main() {
             << is_palindrome(str_a, 0, str_a.size() - 1) << "\n";
   std::cout << "is_palindrome:\t" << str_b << "\t-->\t"
             << is_palindrome(str_b, 0, str_b.size() - 1) << "\n";
+
+  std::cout << "is_palindrome_a:\t" << str_a << "\t-->\t"
+            << is_palindrome_a(str_a, 0, str_a.size() - 1) << "\n";
+  std::cout << "is_palindrome_a:\t" << str_b << "\t-->\t"
+            << is_palindrome_a(str_b, 0, str_b.size() - 1) << "\n";
   return 0;
 }
